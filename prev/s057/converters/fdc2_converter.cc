e0a4944c4f0c5500f80d77b2117e70c8893f6f91
kondo <kondo@ribf.riken.jp>
2026-04-20 19:12:33 +0900

    directory re-organized

---
+++ prev/s057/converters/fdc2_converter.cc
@@ -0,0 +1,41 @@
+#include "SAMURAIDataConverter.hh"
+#include "BDCDataProcessor.hh"
+#include "FDC1DataProcessor.hh"
+#include "FDC2DataProcessor.hh"
+#include "PDCDataProcessor.hh"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+  if (argc<2){
+    std::cout<<"usage: fdc2_converter RunNum"
+             <<std::endl;
+    return 0;
+  }
+
+  Int_t nRun = atoi(argv[1]);
+
+  TString fname_ridf(Form("ridf/sdaq04/data%04d.ridf.gz",nRun));
+  TString fname_out(Form("rootfiles/dc/fdc2_%04d.root",nRun));
+
+  SAMURAIDataConverter converter;
+  //converter.SetMaxEventNumber(100);// temp for check
+
+  converter.SetRIDFFileName(fname_ridf.Data());
+  converter.SetOutputFileName(fname_out.Data());
+
+  converter.SetOutputTreeName("t_FDC2");
+
+  const char *TDCdist = "rootfiles/dctdc/run0309_tdcSpectrum.root";
+
+  FDC2DataProcessor fdc2(TDCdist);
+  fdc2.MakeTreeBranches(true);
+  fdc2.MakeHistograms(true);
+  converter.RegisterDataProcessor(&fdc2);
+
+  converter.Run();
+
+  return 0;
+}
