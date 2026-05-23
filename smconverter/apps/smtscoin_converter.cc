e78156867dd23e626d857d8c87dd5c392cb0c75f
kondo <kondo@ribf.riken.jp>
2026-05-23 15:55:50 +0900

    *updated*

---
+++ smconverter/apps/smtscoin_converter.cc
@@ -0,0 +1,35 @@
+#include "SAMURAIDataConverter.hh"
+#include "SAMURAITSDataProcessor.hh"
+#include "CoinDataProcessor.hh"
+#include <iostream>
+#include <TString.h>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+  if (argc < 3){
+    std::cout << "Usage: bdc_converter input.ridf output.root" << std::endl;
+    return 1;
+  }
+
+  TString fname_ridf = argv[1];
+  TString fname_out  = argv[2];
+  
+  SAMURAIDataConverter converter;
+  converter.SetRIDFFileName(fname_ridf.Data());
+  converter.SetOutputFileName(fname_out.Data());
+  converter.SetOutputTreeName("t_TS");
+
+  SAMURAITSDataProcessor smts;
+  smts.MakeTreeBranches(true);
+
+  CoinDataProcessor coin;
+  coin.MakeTreeBranches(true);
+
+  converter.RegisterDataProcessor(&smts);
+  converter.RegisterDataProcessor(&coin);
+  converter.Run();
+
+  return 0;
+}
